5900ccb6b56053e866a563112d9f5a5614ba4b56
Emanuele Cozzolino <e.cozzolino@accenture.com>
2020-06-01 14:56:35 +0200

    primo commit

---
+++ src/main/java/com/collaborative/gsuite/collaborativegsuite/CollaborativeApplication.java
@@ -0,0 +1,13 @@
+package com.collaborative.gsuite.collaborativegsuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collaborative/gsuite/collaborativegsuite/CollaborativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collaborative.gsuite.collaborativegsuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
